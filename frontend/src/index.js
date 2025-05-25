430afe6166fa25894e1b06053e8bb46316cc668a
Syarif <121801454+syariffff@users.noreply.github.com>
2025-05-25 21:41:50 +0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import RouterApp from './routes/RouterApp';
-import { AuthProvider } from './auth/AuthProvider';
-import AxiosInterceptor from './api/axiosInterceptor';
-import 'bulma/css/bulma.min.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <AxiosInterceptor />
-      <RouterApp />
-    </AuthProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
